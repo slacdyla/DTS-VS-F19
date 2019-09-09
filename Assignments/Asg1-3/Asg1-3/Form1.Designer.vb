f72aca94adcd03c4f3d3da1aa2b899649c3c3060
Dylan Slack <slacddyla@isu.edu>
2019-09-09 08:07:44 -0600

    Wisdom Machine
    Added Wisom Machine Code for Asg 1-3

--- Assignments/Asg1-3/Asg1-3/Form1.Designer.vb
+++ Assignments/Asg1-3/Asg1-3/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,76 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PageSetupDialog1 = New System.Windows.Forms.PageSetupDialog()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.WisText = New System.Windows.Forms.Label()
+        Me.WisBut = New System.Windows.Forms.Button()
+        Me.ExitButt = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Panel1.Controls.Add(Me.WisText)
+        Me.Panel1.Location = New System.Drawing.Point(65, 52)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(630, 55)
+        Me.Panel1.TabIndex = 0
+        '
+        'WisText
+        '
+        Me.WisText.AutoSize = True
+        Me.WisText.BackColor = System.Drawing.Color.Transparent
+        Me.WisText.Font = New System.Drawing.Font("MV Boli", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WisText.Location = New System.Drawing.Point(18, 12)
+        Me.WisText.Name = "WisText"
+        Me.WisText.Size = New System.Drawing.Size(176, 26)
+        Me.WisText.TabIndex = 0
+        Me.WisText.Text = "Wisdoms Machine"
+        '
+        'WisBut
+        '
+        Me.WisBut.BackColor = System.Drawing.Color.Transparent
+        Me.WisBut.Location = New System.Drawing.Point(134, 326)
+        Me.WisBut.Name = "WisBut"
+        Me.WisBut.Size = New System.Drawing.Size(93, 58)
+        Me.WisBut.TabIndex = 1
+        Me.WisBut.Text = "Press For Wisdom"
+        Me.WisBut.UseVisualStyleBackColor = False
+        '
+        'ExitButt
+        '
+        Me.ExitButt.BackColor = System.Drawing.Color.Transparent
+        Me.ExitButt.Location = New System.Drawing.Point(602, 326)
+        Me.ExitButt.Name = "ExitButt"
+        Me.ExitButt.Size = New System.Drawing.Size(93, 58)
+        Me.ExitButt.TabIndex = 1
+        Me.ExitButt.Text = "EXIT"
+        Me.ExitButt.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkGray
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButt)
+        Me.Controls.Add(Me.WisBut)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PageSetupDialog1 As PageSetupDialog
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents WisText As Label
+    Friend WithEvents WisBut As Button
+    Friend WithEvents ExitButt As Button
 End Class
--- Assignments/Asg1-3/Asg1-3/Form1.vb
+++ Assignments/Asg1-3/Asg1-3/Form1.vb
@@ -1,3 +1,47 @@
-﻿Public Class Form1
+﻿'Assignment: Asg1-3
+'RCET0265
+'Dylan Slack
+'Wisdom Program
 
+
+Public Class Form1
+    Dim curWis As String
+    Dim wisFour As String = "I Smell Like Beef"
+    Dim wisThree As String = "In Case of Emergency, Break Glass"
+    Dim wisTwo As String = "I am Beyonce, Always"
+    Dim wisOne As String = "The Mitochondria is the powerhouse of the cell"
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles WisText.Click
+
+    End Sub
+
+    Private Sub WisBut_Click(sender As Object, e As EventArgs) Handles WisBut.Click
+
+        WisText.Text = curWis
+
+        If curWis = wisFour Then
+            curWis = wisOne
+        ElseIf curWis = wisOne Then
+            curWis = wisTwo
+        ElseIf curWis = wisTwo Then
+            curWis = wisThree
+        ElseIf curWis = wisThree Then
+            curWis = wisFour
+        Else
+            curWis = wisOne
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub ExitButt_Click(sender As Object, e As EventArgs) Handles ExitButt.Click
+        End
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
